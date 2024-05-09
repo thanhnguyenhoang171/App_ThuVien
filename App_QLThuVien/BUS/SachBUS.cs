333818d0af92ff05fa0ce2e0a43a8b2ef334af2e
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2024-05-09 15:42:06 +0700

    add 3:42 5/9

---
+++ App_QLThuVien/BUS/SachBUS.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAO;
+
+namespace BUS
+{
+    public class SachBUS
+    {
+        private static SachBUS instance;
+        public static SachBUS Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                    instance = new SachBUS();
+                return instance;
+            }
+        }
+        private SachBUS() { }
+        public  void Xem(DataGridView data)
+        {
+            // Xóa tất cả các cột trong DataGridView trước khi thêm mới
+            data.Columns.Clear();
+
+            // Thiết lập AutoGenerateColumns thành false để ngăn DataGridView tự động tạo cột
+            data.AutoGenerateColumns = false;
+
+            // Tạo cột cho Mã Sách
+            DataGridViewTextBoxColumn colMaSach = new DataGridViewTextBoxColumn();
+            colMaSach.DataPropertyName = "MaSach";
+            colMaSach.HeaderText = "Mã Sách";
+            data.Columns.Add(colMaSach);
+
+            // Tạo cột cho Tên Sách
+            DataGridViewTextBoxColumn colTenSach = new DataGridViewTextBoxColumn();
+            colTenSach.DataPropertyName = "TenSach";
+            colTenSach.HeaderText = "Tên Sách";
+            data.Columns.Add(colTenSach);
+
+            // Tạo cột cho Tác Giả
+            DataGridViewTextBoxColumn colTacGia = new DataGridViewTextBoxColumn();
+            colTacGia.DataPropertyName = "TenTacGia";
+            colTacGia.HeaderText = "Tác Giả";
+            data.Columns.Add(colTacGia);
+
+            // Tạo cột cho Thể Loại
+            DataGridViewTextBoxColumn colTheLoai = new DataGridViewTextBoxColumn();
+            colTheLoai.DataPropertyName = "TheLoai";
+            colTheLoai.HeaderText = "Thể Loại";
+            data.Columns.Add(colTheLoai);
+
+            // Tạo cột cho Số Lượng Hiện Có
+            DataGridViewTextBoxColumn colSoLuong = new DataGridViewTextBoxColumn();
+            colSoLuong.DataPropertyName = "SoLuong";
+            colSoLuong.HeaderText = "Số Lượng Hiện Có";
+            data.Columns.Add(colSoLuong);
+
+            // Tạo cột cho Mô Tả
+            DataGridViewTextBoxColumn colMoTa = new DataGridViewTextBoxColumn();
+            colMoTa.DataPropertyName = "MoTa";
+            colMoTa.HeaderText = "Mô Tả";
+            data.Columns.Add(colMoTa);
+
+            // Tạo cột cho Giá
+            DataGridViewTextBoxColumn colGia = new DataGridViewTextBoxColumn();
+            colGia.DataPropertyName = "GiaSach";
+            colGia.HeaderText = "Giá";
+            data.Columns.Add(colGia);
+
+            // Tạo cột cho Năm Xuất Bản
+            DataGridViewTextBoxColumn colNamXB = new DataGridViewTextBoxColumn();
+            colNamXB.DataPropertyName = "NamXB";
+            colNamXB.HeaderText = "Năm Xuất Bản";
+            data.Columns.Add(colNamXB);
+
+            data.DataSource = SachDAO.Instance.Xem();
+        }
+    }
+}
---
+++ App_QLThuVien/GUI_APP_QLThuVien/FormMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+
+namespace GUI_APP_QLThuVien
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void dgvSach_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnXemSach_Click(object sender, EventArgs e)
+        {
+            SachBUS.Instance.Xem(dgvSach);
+        }
+    }
+}
