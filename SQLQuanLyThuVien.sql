7dabf26be1b7e5054f7a857a8e5aa93e7cb33a7a
minhhkhue <khueminh0210@gmail.com>
2024-05-23 18:25:24 +0700

    Sửa các lỗi và ràng buộc

--- SQLQuanLyThuVien.sql
+++ SQLQuanLyThuVien.sql
@@ -11,25 +11,26 @@ CREATE TABLE Sach (
     ten_sach NVARCHAR(255),
     tac_gia NVARCHAR(100),
     the_loai NVARCHAR(150),
+	mo_ta NVARCHAR(150),
     so_luong_hien_co INT CHECK (so_luong_hien_co >= 0),
-    mo_ta NVARCHAR(150),
+    
     gia DECIMAL(10, 2) CHECK (gia >= 0),
     nam_xuat_ban INT CHECK (nam_xuat_ban >= 0)
 );
 GO
 -- Thêm dữ liệu vào bảng Sách
-INSERT INTO Sach (ma_sach, ten_sach, tac_gia, the_loai, so_luong_hien_co, mo_ta, gia, nam_xuat_ban)
+INSERT INTO Sach (ma_sach, ten_sach, tac_gia, the_loai, mo_ta, so_luong_hien_co, gia, nam_xuat_ban)
 VALUES 
-	('S001', N'Harry Potter', N'J.K. Rowling', N'Fantasy', 100, N'Cuốn sách kể về thế giới phù thủy', 15.99, 2001),
-	('S002', N'Nhà giả kim', N'Paulo Coelho', N'Tiểu thuyết', 50, N'Cuốn sách về sự tìm kiếm ý nghĩa cuộc sống', 12.5, 1988),
-    ('S003', N'Sức mạnh của ngôn từ', N'Don Miguel Ruiz', N'Phát triển bản thân', 30, N'Tác phẩm về sức mạnh của từ ngữ', 10.99, 1997),
-    ('S004', N'Nghệ thuật sống', N'Thích Nhất Hạnh', N'Tâm lý - Tâm linh', 20, N'Sách về nghệ thuật sống hạnh phúc', 9.75, 2012),
-	 ('S005', N'Gatsby Vĩ Đại', N'F. Scott Fitzgerald', N'Kinh điển', 40, N'Tiểu thuyết được đặt trong thập niên hồi sinh sức sống', 9.99, 1925),
-    ('S006', N'Tiểu thư Hobbit', N'J.R.R. Tolkien', N'Fantasy', 60, N'Tiểu thuyết phiêu lưu giả tưởng', 14.50, 1937),
-    ('S007', N'Kẻ Bắt Cỏ', N'J.D. Salinger', N'Tiểu thuyết lớn lên', 70, N'Tiểu thuyết về lo âu của tuổi teen', 10.25, 1951),
-	 ('S008', N'Đắc nhân tâm', N'Dale Carnegie', N'Tự phát triển', 90, N'Trải nghiệm về kỹ năng giao tiếp và quản lý tâm trí', 8.75, 1936),
-    ('S009', N'Giáo dục', N'Emile', N'Khoa học xã hội', 55, N'Giáo trình về giáo dục và phát triển cá nhân', 10.50, 1762),
-    ('S010', N'Tiếng chim hót trong bụi mận gai', N'Harper Lee', N'Tiểu thuyết', 65, N'Truyện về sự vô tội và bất công', 13.25, 1960);
+	('S001', N'Harry Potter', N'J.K. Rowling', N'Fantasy', N'Cuốn sách kể về thế giới phù thủy', 100, 15.99, 2001),
+	('S002', N'Nhà giả kim', N'Paulo Coelho', N'Tiểu thuyết', N'Cuốn sách về sự tìm kiếm ý nghĩa cuộc sống', 50, 12.5, 1988),
+    ('S003', N'Sức mạnh của ngôn từ', N'Don Miguel Ruiz', N'Phát triển bản thân', N'Tác phẩm về sức mạnh của từ ngữ', 30, 10.99, 1997),
+    ('S004', N'Nghệ thuật sống', N'Thích Nhất Hạnh', N'Tâm lý - Tâm linh', N'Sách về nghệ thuật sống hạnh phúc', 20, 9.75, 2012),
+	('S005', N'Gatsby Vĩ Đại', N'F. Scott Fitzgerald', N'Kinh điển', N'Tiểu thuyết được đặt trong thập niên hồi sinh sức sống', 40, 9.99, 1925),
+    ('S006', N'Tiểu thư Hobbit', N'J.R.R. Tolkien', N'Fantasy', N'Tiểu thuyết phiêu lưu giả tưởng', 60, 14.50, 1937),
+    ('S007', N'Kẻ Bắt Cỏ', N'J.D. Salinger', N'Tiểu thuyết lớn lên', N'Tiểu thuyết về lo âu của tuổi teen', 70, 10.25, 1951),
+	('S008', N'Đắc nhân tâm', N'Dale Carnegie', N'Tự phát triển', N'Trải nghiệm về kỹ năng giao tiếp và quản lý tâm trí', 90, 8.75, 1936),
+    ('S009', N'Giáo dục', N'Emile', N'Khoa học xã hội', N'Giáo trình về giáo dục và phát triển cá nhân', 55, 10.50, 1762),
+    ('S010', N'Tiếng chim hót trong bụi mận gai', N'Harper Lee', N'Tiểu thuyết', N'Truyện về sự vô tội và bất công', 65, 13.25, 1960);
 
 GO
 -- Tạo bảng "Thẻ thư viện"
@@ -41,13 +42,14 @@ CREATE TABLE The_thu_vien (
     email VARCHAR(255),
     ngay_dang_ky DATE,
     ngay_het_han DATE,
+    CONSTRAINT chk_ngay CHECK (ngay_het_han > ngay_dang_ky)
 );
 
 -- Thêm dữ liệu vào bảng Thẻ thư viện
 INSERT INTO The_thu_vien (ma_the, ho_va_ten, dia_chi, so_dien_thoai, email, ngay_dang_ky, ngay_het_han)
 VALUES 
-    ('T001', N'Nguyễn Văn A', N'123 Đường ABC, Quận XYZ', '0987654321', 'nguyenvana@example.com', '2024-05-01', '2025-05-01'),
-    ('T002', N'Trần Thị B', N'456 Đường XYZ, Quận ABC', '0123456789', 'tranthib@example.com', '2024-05-15', '2025-05-15'),
+    ('T001', N'Nguyễn Lê Minh Khuê', N'123 Đường ABC, Quận XYZ', '0987654321', 'nguyenkhue@example.com', '2024-05-01', '2025-05-01'),
+    ('T002', N'Nguyễn Hoàng Thanh', N'456 Đường XYZ, Quận ABC', '0123456789', 'hthanh@example.com', '2024-05-15', '2025-05-15'),
 	('T003', N'Trần Văn C', N'789 Đường XYZ, Quận DEF', '0321654987', 'tranvanc@example.com', '2024-06-10', '2025-06-10'),
     ('T004', N'Lê Thị D', N'101 Đường GHI, Quận KLM', '0369852147', 'lethid@example.com', '2024-06-15', '2025-06-15'),
     ('T005', N'Phạm Văn E', N'246 Đường MNO, Quận PQR', '0987456321', 'phamvane@example.com', '2024-07-01', '2025-07-01'),
@@ -114,7 +116,7 @@ ADD CONSTRAINT UK_ma_giao_dich UNIQUE (ma_giao_dich);
 
 GO
 
--- Ràng buộc xoá thể thì xoá mượn trả
+-- Ràng buộc xoá thẻ thư viện thì xoá mượn trả
 ALTER TABLE Muon_tra
 DROP CONSTRAINT IF EXISTS FK_ma_the;
 
@@ -251,7 +253,7 @@ BEGIN
 END
 GO
 
--- Tạo một trình kích hoạt để thực thi rằng ngày hết hạn của thẻ phải sau ngày hiện tại cho một giao dịch vay mới
+-- Tạo một trigger để thực thi rằng ngày hết hạn của thẻ phải sau ngày hiện tại cho một giao dịch vay mới
 
 IF OBJECT_ID('trg_CheckExpirationDate') IS NOT NULL
 DROP TRIGGER trg_CheckExpirationDate;
